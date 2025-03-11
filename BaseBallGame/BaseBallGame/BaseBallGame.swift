6ba205af2b40c61167c2c2c60aaef55838b76a69
SongKyuSeob <sks5075@naver.com>
2025-03-11 11:41:57 +0900

    feat: 온보딩 메뉴 선택 기능 추가

--- BaseBallGame/BaseBallGame/BaseBallGame.swift
+++ BaseBallGame/BaseBallGame/BaseBallGame.swift
@@ -9,38 +9,56 @@ import Foundation
 
 struct BaseBallGame {
     func start() {
-        let answer = makeAnswer()
-        var repeatedCount = 0
-        var myStrikeList = [Int]()
-        
-        print(answer)
         
         while true {
-            if repeatedCount == 0 { print("< 게임을 시작합니다 >") }
             
-            repeatedCount += 1
+            // MARK: - Onboarding
+            print("환영합니다! 원하시는 번호를 입력해주세요")
+            print("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+            let mode = readLine()
             
-            print("숫자를 입력하세요")
-            let myNumber = readLine()
-            
-            guard let input = myNumber, let intTypeInput = Int(input), intTypeInput >= 100, intTypeInput <= 999 else {
+            switch mode {
+            case "1":
+                var repeatedCount = 0
+                var myStrikeList = [Int]()
+                let answer = makeAnswer()
+                
+                while true {
+                    
+                    if repeatedCount == 0 { print("< 게임을 시작합니다 >") }
+                    
+                    repeatedCount += 1
+                    
+                    // MARK: - 사용자로부터 입력 값 입력 및 유효성 검증
+                    print("숫자를 입력하세요")
+                    let myNumber = readLine()
+                    guard let input = myNumber, let intTypeInput = Int(input), intTypeInput >= 100, intTypeInput <= 999 else {
+                        print("올바르지 않은 입력값입니다.")
+                        continue
+                    }
+                    
+                    // MARK: - 기존 정답과 인풋에 대한 스트라이크, 볼, 정답 여부 확인 로직
+                    let answerString = String(answer)
+                    
+                    myStrikeList = findStrike(answer: answerString, myNum: input)
+                    
+                    let ballCount = findBall(answer: answerString, myNum: input, myStrikeList: myStrikeList)
+                    
+                    if myStrikeList.count == 3 {
+                        print("정답입니다!")
+                        break
+                    } else {
+                        print("\(myStrikeList.count) 스트라이크, \(ballCount) 볼")
+                    }
+                }
+                
+            case "2":
+                print("미구현 기능입니다.")
+            case "3":
+                return
+            default:
                 print("올바르지 않은 입력값입니다.")
-                continue
             }
-            
-            let answerString = String(answer)
-            
-            myStrikeList = findStrike(answer: answerString, myNum: input)
-            
-            let ballCount = findBall(answer: answerString, myNum: input, myStrikeList: myStrikeList)
-            
-            if myStrikeList.count == 3 {
-                print("정답입니다!")
-                break
-            } else {
-                print("\(myStrikeList.count) 스트라이크, \(ballCount) 볼")
-            }
-            
         }
     }
     
@@ -52,7 +70,7 @@ struct BaseBallGame {
             let tensNum = (answer - (hundredsNum * 100)) / 10
             let oneNum = (answer - (hundredsNum * 100)) % 10
             
-            guard (hundredsNum != tensNum) && (tensNum != oneNum) && (hundredsNum != oneNum) else {
+            guard (hundredsNum != tensNum) && (tensNum != oneNum) && (hundredsNum != oneNum) && (hundredsNum != 0) else {
                 continue
             }
             
@@ -91,4 +109,5 @@ struct BaseBallGame {
         
         return ballCount
     }
+    
 }
