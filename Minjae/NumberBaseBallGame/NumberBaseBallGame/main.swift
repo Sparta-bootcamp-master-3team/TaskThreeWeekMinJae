43f523296df48893459a544049cae253a0f9413d
minjae <qlqlzbto@naver.com>
2025-03-12 12:41:25 +0900

    feat: protocol init

--- Minjae/NumberBaseBallGame/NumberBaseBallGame/main.swift
+++ Minjae/NumberBaseBallGame/NumberBaseBallGame/main.swift
@@ -1,11 +1,15 @@
-//
-//  main.swift
-//  NumberBaseBallGame
-//
-//  Created by main on 3/11/25.
-//
-
 import Foundation
 
-print("Hello, World!")
+// 게임모드 (시작, 기록보기, 종료)
+protocol GameContentable: AnyObject {
+    func gameStart(currentGameIndex: Int) -> (game: Int, tryCount: Int)
+    func openRecord(records: [(game: Int, tryCount: Int)])
+    func gameExit()
+}
 
+// 게임의 전반적인 로직 (정답생성, 입력값 확인, 숫자 비교)
+protocol GameLogicProtocol: AnyObject {
+    func initAnswerIntegerArray() -> [Int]
+    func isCorrectInput(in input: String) -> Bool
+    func compare(input: [Int], withAnswer answer: [Int]) -> (equalPoint: Int, containsPoint: Int)
+}
