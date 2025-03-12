40d447681e9cb0598786b55ca96044c4fa583680
KwakDADA <down022@gmail.com>
2025-03-12 17:40:01 +0900

    refactor: GameMenu 파일 분리

---
+++ daeun/BaseballGame/BaseballGame/Domain/GameMenu.swift
@@ -0,0 +1,12 @@
+//
+//  GameMenu.swift
+//  BaseballGame
+//
+//  Created by 곽다은 on 3/12/25.
+//
+
+enum GameMenu: Int, CaseIterable {
+    case start = 1
+    case log
+    case end
+}
--- daeun/BaseballGame/BaseballGame/UseCases/MenuInputValidator.swift
+++ daeun/BaseballGame/BaseballGame/UseCases/MenuInputValidator.swift
@@ -5,12 +5,6 @@
 //  Created by 곽다은 on 3/12/25.
 //
 
-enum GameMenu: Int, CaseIterable {
-    case start = 1
-    case log
-    case end
-}
-
 struct MenuInputValidator: Validatable {
     func validate(input: String) throws {
         guard let inputNumber = Int(input),
