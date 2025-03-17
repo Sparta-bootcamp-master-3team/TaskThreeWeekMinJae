740cac333ee19b36a757df9604fc27654e618590
minjae <qlqlzbto@naver.com>
2025-03-17 10:34:54 +0900

    feat: remove dir

--- Minjae/NumberBaseBallGame/NumberBaseBallGame/main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-// 프로토콜 주입 및 게임 실행
-let gameLogic = GameLogic()
-let gameContent = GameContent()
-let gameManager = BaseBallGameManager()
-gameContent.delegate = gameLogic
-gameManager.gameContent = gameContent
-gameManager.start()
