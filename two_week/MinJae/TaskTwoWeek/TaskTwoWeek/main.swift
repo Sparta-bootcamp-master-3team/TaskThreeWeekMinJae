f73366d3ce8bd15e3fc2e5dfa70cd0fbaf3bb127
minjae <qlqlzbto@naver.com>
2025-03-17 17:37:09 +0900

    feat: 도전 문제 1

--- two_week/MinJae/TaskTwoWeek/TaskTwoWeek/main.swift
+++ two_week/MinJae/TaskTwoWeek/TaskTwoWeek/main.swift
@@ -75,3 +75,87 @@ func d<T: Numbers>(_ arr: [T]) -> [T] {
 }
 
 
+
+// MARK: 도전 문제 1
+class EngineClass { }
+class ElectricEngineClass: EngineClass { }
+class HydrogenEngineClass: EngineClass { }
+class CarClass {
+    var brand: String
+    var model: String
+    var year: String
+    var engine: EngineClass
+    
+    init(brand: String, model: String, year: String, engine: EngineClass) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+    }
+    
+    func drive() {
+        print("Car 주행 중")
+    }
+    
+    func stop() {
+        print("정지")
+    }
+    
+    func charge() {
+        print("충전 중")
+    }
+    
+    func refuel() {
+        print("주유 중")
+    }
+}
+
+class ElectricCarClass: CarClass { }
+class HybridCarClass: CarClass {
+    func switchEngine(to engine: EngineClass) {
+        self.engine = engine
+    }
+}
+let ec = ElectricCarClass(brand: "hi", model: "hi", year: "hi", engine: ElectricEngineClass())
+var hc = HybridCarClass(brand: "hi", model: "hi", year: "hi", engine: HydrogenEngineClass())
+print(hc.engine)
+hc.switchEngine(to: ElectricEngineClass())
+print(hc.engine)
+
+protocol EngineProtocol { }
+protocol ElectricEngineProtocol: EngineProtocol { }
+protocol HydrogenEngineProtocol: EngineProtocol { }
+
+protocol CarProtocol {
+    var brand: String { get set }
+    var model: String { get set }
+    var year: String { get set }
+    var engine: EngineProtocol { get set }
+}
+
+protocol ElectricCarProtocol: CarProtocol { }
+protocol HybridCarProtocol: CarProtocol {
+    func switchEngine(to Engine: EngineProtocol)
+}
+
+class MinjaeCar: HybridCarProtocol {
+    func switchEngine(to Engine: EngineProtocol) {
+        self.engine = Engine
+    }
+    
+    var brand: String
+    var model: String
+    var year: String
+    var engine: EngineProtocol
+    
+    init(brand: String, model: String, year: String, engine: EngineProtocol) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+    }
+}
+
+class MinjaeCarEngine: HydrogenEngineProtocol { }
+var minjaeCar = MinjaeCar(brand: "kia", model: "model", year: "2025", engine: MinjaeCarEngine())
+print(minjaeCar.engine)
