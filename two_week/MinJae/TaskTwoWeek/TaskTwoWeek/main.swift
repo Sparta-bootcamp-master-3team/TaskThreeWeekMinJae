2a4258c9c234172ee346f58d5389ca2cb79615c0
minjae <qlqlzbto@naver.com>
2025-03-17 15:48:52 +0900

    feat: 문제 1

--- two_week/MinJae/TaskTwoWeek/TaskTwoWeek/main.swift
+++ two_week/MinJae/TaskTwoWeek/TaskTwoWeek/main.swift
@@ -1,11 +1,16 @@
-//
-//  main.swift
-//  TaskTwoWeek
-//
-//  Created by main on 3/17/25.
-//
-
 import Foundation
 
-print("Hello, World!")
+// MARK: 문제 1
+//var str: ((Int, Int) -> String) =
+
+var sum: ((Int, Int) -> String) = { (num1: Int, num2: Int) in
+    return "두 수의 합은 \(num1 + num2)입니다."
+}
+print(sum(10, 20))
+
+func calculate(_ closure: (Int, Int) -> String ) { }
+
+calculate { num1, num2 in
+    return "\(num1 + num2)"
+}
 
