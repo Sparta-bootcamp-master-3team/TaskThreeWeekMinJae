ce78f76cebb36d0c4f7359bb7ad8bd08115497d8
minjae <qlqlzbto@naver.com>
2025-03-20 14:55:08 +0900

    feat: comment ThirdQuestion

--- two_week/MinJae/TaskTwoWeek/TaskTwoWeek/ThirdQuestion.swift
+++ two_week/MinJae/TaskTwoWeek/TaskTwoWeek/ThirdQuestion.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class ThirdQuestion {
     
+    // Int배열의 짝수번째 원소를 제거하여 리턴
     private func a(_ arr: [Int]) -> [Int] {
         var output = [Int]()
         arr.enumerated().forEach { (index, element) in
@@ -15,7 +16,7 @@ class ThirdQuestion {
         let result = a(input)
         return result
     }
-
+    // String배열의 짝수번째 원소를 제거하여 리턴
     private func b(_ arr: [String]) -> [String] {
         var output = [String]()
         arr.enumerated().forEach { (index, element) in
@@ -29,7 +30,7 @@ class ThirdQuestion {
         return b(input)
     }
     
-
+    // 제네릭을 활용하여 Int, String 타입 모두 파라미터로 받을 수 있도록 구현
     private func c<T>(_ arr: [T]) -> [T] {
         var output = [T]()
         arr.enumerated().forEach { (index, element) in
@@ -45,6 +46,8 @@ class ThirdQuestion {
         return (c(input1), c(input2))
     }
     
+    // Numeric을 채택한 타입이 파라미터로 입력받고 리턴하는 함수
+    // 인덱스의 홀수번째를 제거하고 리턴
     private func d<T: Numeric>(_ arr: [T]) -> [T] {
         var output = [T]()
         arr.enumerated().forEach { (index, element) in
